9be5f536ed8e7ed24bc4c33f432b32c79988eec8
jaciara batista <andreandrade1920@gmail.com>
2025-06-12 22:14:58 -0300

    'api'

--- src/extractTransform.py
+++ src/extractTransform.py
@@ -2,11 +2,14 @@
 import requests
 import pandas as pd
 
-def requestTaxaJuros():
+def requestTaxaJuros(inicio: str, fim: str):
     """
     Extrai dados da API de Taxa de Juros do BCB e retorna um DataFrame.
     """
-    url = "https://olinda.bcb.gov.br/olinda/servico/taxaJuros/versao/v2/odata/ConsultaUnificada?$top=10000&$format=json"
+
+    filtro = f"IniciarPeriodo ge '{inicio} and FimPeriodo le '{fim}'"
+    filtro_encoded = requests.utils.quote(filtro)
+    url = f"https://olinda.bcb.gov.br/olinda/servico/taxaJuros/versao/v2/odata/ConsultaUnificada?$top=1000000&$format=json&$filter={filtro_encoded}"
     response = requests.get(url)
 
     if response.status_code == 200:
